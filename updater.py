1b623d28cae10a410507ba6c355bdc5e38a2e23a
cangcang <cangcang_code@icloud.com>
2026-04-26 20:30:44 +0800

    提交

---
+++ updater.py
@@ -0,0 +1,10 @@
+from Scripts import resource_fetcher
+from Scripts import github
+from Scripts import run
+from Scripts import utils
+import os
+import tempfile
+import shutil
+
+class Updater:
+    print("更新功能未实现")
